e74d0b3a99fc0c98b3b30e5bd954a5b2f22dc053
Annie Lace <annie.c.lace@gmail.com>
2016-04-04 22:02:43 -0700

    SimpleCalc complete

---
+++ SimpleCalc/SimpleCalc/main.swift
@@ -0,0 +1,63 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Annie Lace on 4/4/16.
+//  Copyright © 2016 Annie Lace. All rights reserved.
+//
+
+import Foundation
+
+//For Standard input reading
+func input() -> String {
+    let keyboard = NSFileHandle.fileHandleWithStandardInput()
+    let inputData = keyboard.availableData
+    let result = NSString(data: inputData, encoding:NSUTF8StringEncoding) as! String
+    return result.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+}
+
+//For converting strings to numbers
+func convert(incoming:String) -> Int {
+    return NSNumberFormatter().numberFromString(incoming)!.integerValue
+}
+
+func fact(number:Int) -> Int
+{
+    var result = 1
+    for(var i = number - 1; i > 1; i -= 1)
+    {
+        result = result * number;
+    }
+    return result;
+}
+print("Enter an expression separated by returns:")
+
+var firstNum = convert(input());
+
+var next = input();
+switch next
+{
+    case "+": print("Result: \(firstNum + convert(input()))")
+    case "-": print("Result: \(firstNum - convert(input()))")
+    case "*": print("Result: \(firstNum * convert(input()))")
+    case "/": print("Result: \(firstNum / convert(input()))")
+    case "%": print("Result: \(firstNum % convert(input()))")
+    case "fact": print("Result: \(fact(firstNum))")
+    default: var count = 2
+             var sum = firstNum + convert(next)
+             next = input()
+             while(next != "count" && next != "avg")
+             {
+                count += 1
+                sum += convert(next)
+                next = input()
+             }
+             if(next == "count")
+             {
+                print("Result: \(count)")
+             }
+             else
+             {
+                print("Result: \(sum / count)")
+             }
+}
